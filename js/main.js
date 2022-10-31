aa33e24db20ad7d473c44ed49ad13ea9b18f71d3
marialapc <marialopez.apc@gmail.com>
2022-10-31 09:17:14 +0100

    get a random number

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 'use strict'
 
+
+//variables
+const btn = document.querySelector('.js-btn');
+const select = document.querySelector('.js-select');
+const result = document.querySelector('.js-result');
+
+
+
+
+//funciones
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
+
+   const computer = () => {
+    const randomNUmber = getRandomNumber(6);
+    console.log(randomNUmber);
+}
+
+
+//funcion manejadora
+function handleClick(e) {
+    e.preventDefault();
+    computer();
+}
+  
+//evento
+   btn.addEventListener('click', handleClick);
